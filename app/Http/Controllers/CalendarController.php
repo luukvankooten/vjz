f52cb337b0e703940a65eb985c7a0b47e3ce716d
Luuk van Kooten <luuk@anonymous.local>
2018-06-25 16:46:55 +0200

    forget to push this file

--- app/Http/Controllers/CalendarController.php
+++ app/Http/Controllers/CalendarController.php
@@ -2,16 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use Carbon\Carbon;
 use Illuminate\Http\Request;
-use App\{Role};
+use App\{
+    Calendar, Role, User
+};
 
 class CalendarController extends Controller
 {
 
     function __construct()
     {
-        $this->middleware(['auth', 'can:calender']);
-        $this->middleware('can:create-appointments')->only('create');
+        $this->middleware(['auth', 'can:agenda']);
+        $this->middleware('can:afspraken')->only(['create', 'store']);
     }
 
     /**
@@ -33,9 +36,23 @@ class CalendarController extends Controller
     {
         $request->validate([
             'users' => 'required|string|exists:users,name',
-            'date' => 'required|date',
-            'time' => 'required|date',
-            'description' => 'required|string',
+            'date' => 'required|date_format:"Y-m-d"',
+            'time' => 'required|date_format:"H:i"',
+            'title' => 'required|string|max:255',
+            'description' => 'required|string|max:16777215',
         ]);
+
+        $user = User::whereName($request->users)->first()->id;
+        $date = (new Carbon($request->date . $request->time))->toDateTimeString();
+
+        Calendar::create([
+            'user_id' => $user,
+            'creator_id' => auth()->user()->id,
+            'datetime' => $date,
+            'title' => $request->title,
+            'description' => $request->description,
+        ]);
+
+        return redirect('afspraak')->with('status', 'Afspraak succesvol aangemaakt.');
     }
 }
